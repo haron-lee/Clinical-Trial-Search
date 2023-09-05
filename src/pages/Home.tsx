0844cc97f2d8fca4c46c73a6ad9785bdbe0ca429
haron <haronxlee@gmail.com>
2023-09-05 23:46:02 +0900

    Design: 제목 및 UI 컴포넌트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { styled } from 'styled-components';
+import Form from 'components/Form';
+import SelectList from 'components/Select/SelectList';
 
 const Home: React.FC = () => {
   return (
-    <>
-      <div>Home</div>
-    </>
+    <Wrapper>
+      <h1>
+        국내 모든 임상시험 검색하고 <br /> 온라인으로 참여하기
+      </h1>
+      <Form />
+      <SelectList />
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.main`
+  max-width: 700px;
+  margin: 100px auto;
+
+  h1 {
+    margin-bottom: 20px;
+    font-weight: bold;
+    font-size: 36px;
+    text-align: center;
+    line-height: 1.6;
+  }
+`;
 
 export default Home;
