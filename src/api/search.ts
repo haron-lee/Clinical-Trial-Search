768702b68e8383fe3063e225fdf6a70e6479f32d
haron <haronxlee@gmail.com>
2023-09-08 01:19:31 +0900

    Feat: API 요청 URL 환경변수 값으로 변경

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,9 +1,9 @@
 import Http from 'api/http';
 import { Disease } from 'types';
 
-const URL = 'http://localhost:4000';
+// const URL = 'http://localhost:4000';
 
-const searchHttp = new Http(URL);
+const searchHttp = new Http(`${process.env.REACT_APP_DISEASE_DB}`);
 
 export const getKeyword = async (keyword: string | undefined) => {
   return await searchHttp.get<Disease[]>('/sick?_limit=10', {
